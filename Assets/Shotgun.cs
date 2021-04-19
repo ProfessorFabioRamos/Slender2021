8eae88a82f7e5c1149a94b3134ecc1ec1db02165
Professor Fabio Ramos <fabio.sramos@ceub.edu.br>
2021-04-19 19:17:15 -0300

    14-04-21

--- Assets/Shotgun.cs
+++ Assets/Shotgun.cs
@@ -5,15 +5,35 @@ using UnityEngine;
 public class Shotgun : MonoBehaviour
 {
     public AudioClip[] sfx;
+    private AudioSource aud;
+    public float range = 10;
+    public Camera fpsCam;
+    public LayerMask enemyLayer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        aud = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButtonDown(0)){
+            aud.clip = sfx[1];
+            //aud.PlayOneShot(sfx[1]);
+            aud.Play();
+
+            RaycastHit hit;
+            Vector3 origin = fpsCam.ViewportToWorldPoint(Vector3.zero);
+
+            if(Physics.Raycast(origin, fpsCam.transform.forward, out hit, range)){
+                Debug.Log(hit.transform.name);
+                if(hit.transform.gameObject.tag == "Enemy"){
+                    Destroy(hit.transform.gameObject);
+                }
+                //Debug.DrawLine(firePosition.position, )
+            }
+        }
     }
 }
